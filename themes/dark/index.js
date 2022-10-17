29428164b78fc95167b08f8f37895485a0e91a37
RoiArthurB <contact@arthurbrugiere.fr>
2022-10-17 21:23:19 +0700

    [THEME] Add dark theme vInit

---
+++ themes/dark/index.js
@@ -0,0 +1,77 @@
+// Original: https://github.com/dracula/visual-studio-code
+// Converted automatically by hand
+
+var theme = {
+  plain: {
+    color: "#F8F8F2",
+    backgroundColor: "#282A36",
+  },
+  styles: [
+    {
+      types: ["variable"],
+      style: {
+        color: "rgb(94,198,163)",
+      },
+    },
+    {
+      types: ["operator"],
+      style: {
+        color: "rgb(163,230,34)",
+        fontStyle: "bold",
+      },
+    },
+    {
+      types: ["statement"],
+      style: {
+        color: "rgb(204,108,29)",
+        fontStyle: "bold",
+      },
+    },
+    {
+      types: ["string"],
+      style: {
+        color: "rgb(141,218,248)",
+      },
+    },
+    {
+      types: ["comment"],
+      style: {
+        color: "rgb(56,142,193)",
+      },
+    },
+    {
+      types: ["facet"],
+      style: {
+        color: "rgb(243,159,62)",
+      },
+    },
+    {
+      types: ["constant"],
+      style: {
+        color: "rgb(166,166,166)",
+      },
+    },
+    {
+      types: ["boolean", "number"],
+      style: {
+        color: "rgb(103,150,186)",
+      },
+    },
+    {
+      types: ["keyword"],
+      style: {
+        color: "rgb(166,166,166)",
+        fontStyle: "italic",
+      },
+    },
+    {
+      types: ["type_statement"],
+      style: {
+        color: "rgb(204,108,29)",
+        fontStyle: "bold",
+      },
+    },
+  ],
+};
+
+export default theme;
