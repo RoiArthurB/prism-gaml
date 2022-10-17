5c5820a08198be126a118939baf8338c8b8904cb
RoiArthurB <contact@arthurbrugiere.fr>
2022-10-17 21:20:35 +0700

    [THEME] Add light theme vInit

---
+++ themes/light/index.js
@@ -0,0 +1,77 @@
+// Original: https://github.com/dracula/visual-studio-code
+// Converted automatically by hand
+
+var theme = {
+  plain: {
+    color: "#282A36",
+    backgroundColor: "#FFFFFF",
+  },
+  styles: [
+    {
+      types: ["variable"],
+      style: {
+        color: "rgb(28,125,180)",
+      },
+    },
+    {
+      types: ["operator"],
+      style: {
+        color: "rgb(46,93,78)",
+        fontStyle: "bold",
+      },
+    },
+    {
+      types: ["statement"],
+      style: {
+        color: "rgb(127,0, 85)",
+        fontStyle: "bold",
+      },
+    },
+    {
+      types: ["string"],
+      style: {
+        color: "rgb(116,167,251)",
+      },
+    },
+    {
+      types: ["comment"],
+      style: {
+        color: "rgb(63,127,95)",
+      },
+    },
+    {
+      types: ["facet"],
+      style: {
+        color: "rgb(154,72,71)",
+      },
+    },
+    {
+      types: ["constant"],
+      style: {
+        color: "rgb(0, 0, 0)",
+      },
+    },
+    {
+      types: ["boolean", "number"],
+      style: {
+        color: "rgb(125,125,125)",
+      },
+    },
+    {
+      types: ["keyword"],
+      style: {
+        color: "rgb(0, 0, 0)",
+        fontStyle: "italic",
+      },
+    },
+    {
+      types: ["type_statement"],
+      style: {
+        color: "rgb(127,0,85)",
+        fontStyle: "bold",
+      },
+    },
+  ],
+};
+
+export default theme;
